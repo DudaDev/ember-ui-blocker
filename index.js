b0d83d2ad94b42b6e858ec2ed87ca1fe2af0e37c
Liad Yosef <liady@users.noreply.github.com>
2018-06-04 10:14:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
 	included: function lightbox_included(app) {
 		var addonConfig = this.app.project.config(app.env)['ember-ui-blocker'] || {};
 		
-		this.app.import('bower_components/blockUI/jquery.blockUI.js');
+		this.app.import('bower_components/blockui/jquery.blockUI.js');
 
 		if (addonConfig.includeSpinjs) {
 			this.app.import('bower_components/spin.js/spin.js');
